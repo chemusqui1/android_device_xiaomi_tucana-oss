eadcb769915ee14940003d211c8d6ab1c41c68c1
chemusqui1 <61394758+chemusqui1@users.noreply.github.com>
2021-06-06 13:45:38 +0200

    Rename overlay-lineage to overlay-system

--- device.mk
+++ device.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-system
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay/frameworks/base/packages/SystemUI
