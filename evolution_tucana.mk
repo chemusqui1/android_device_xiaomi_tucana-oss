7ab424ea0fe3492ab754a0e91ec66d0eb295f0a7
chemusqui1 <61394758+chemusqui1@users.noreply.github.com>
2021-06-20 16:47:17 +0200

    Let's get evolved!

--- evolution_tucana.mk
+++ evolution_tucana.mk
@@ -7,14 +7,14 @@
 # Inherit from tucana device
 $(call inherit-product, device/xiaomi/tucana/device.mk)
 
-# Inherit some common PEX stuff.
+# Inherit some common EvoX stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_tucana
+PRODUCT_NAME := evolution_tucana
 PRODUCT_DEVICE := tucana
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
